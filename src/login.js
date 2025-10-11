7efe97a9c23fb384905784acbd682c3a6677ac32
Rodrigo de Carvalho <carvalho_borges@icloud.com>
2025-10-11 15:58:36 -0300

    novo site
    novas config novo layout

---
+++ src/login.js
@@ -0,0 +1,62 @@
+document.getElementById('submitBtn').addEventListener('click', (event) => {
+    // 1. Previne o envio padrão do formulário, que recarregaria a página.
+    event.preventDefault();
+
+    // 2. Elementos do HTML
+    const loginForm = document.getElementById('loginForm');
+    const status = document.getElementById('status');
+    const mensagemLogin = document.getElementById('mensagemLogin');
+
+    // 3. Dados de login corretos (Como strings, o que é CORRETO para inputs.value)
+    const agencyLogin = "1234";
+    const accountLogin = "98765432";
+    const cpfLogin = "1234567890";
+    const passwordLogin = "123456";
+
+    // 4. Obtém os valores atuais dos campos
+    const agency = document.getElementById('agency').value;
+    const account = document.getElementById('account').value;
+    const cpf = document.getElementById('cpf').value;
+    const password = document.getElementById('password').value;
+
+
+
+    // 5. Validação (Ordem CORRETA: Vazio -> Sucesso -> Incorreto)
+
+    if (agency === "" || account === "" || cpf === "" || password === "") {
+        // Exemplo SweetAlert para campos vazios
+        Swal.fire({
+            icon: 'warning',
+            title: 'Atenção!',
+            text: 'Por favor, preencha todos os campos.',
+        });
+
+        } else if (agency === agencyLogin && account === accountLogin && cpf === cpfLogin && password === passwordLogin) {
+        // Login Bem-Sucedido
+        Swal.fire({
+            icon: 'success', // Ícone de sucesso (verde)
+            title: 'Login Bem-Sucedido!',
+            text: 'Você será redirecionado em breve.',
+            showConfirmButton: false, // Não mostra o botão, pois vai redirecionar
+            timer: 1500 // Fecha automaticamente em 1.5 segundos
+            }).then(() => {
+
+            window.location.href = "./index.html";
+
+            });
+        } else {
+            // Exemplo SweetAlert para credenciais incorretas
+            Swal.fire({
+                icon: 'error', // Ícone de erro 
+                title: 'Erro de Login',
+                text: 'Credenciais incorretas. Tente novamente.',
+            });
+        }
+        });
+
+                // ativar a função de criar uma nova conta 
+
+        openAccount.addEventListener('click', (event) => {
+            event.preventDefault();
+            window.location.href = "./newconta.html";
+        });
---
+++ src/newconta.js
@@ -0,0 +1,39 @@
+document.getElementById('submitCadastroBtn').addEventListener('click', (event) => {
+    // 1. Previne o envio padrão do formulário, que recarregaria a página.
+    event.preventDefault();
+
+    const CadastroForm = document.getElementById('cadastroForm');
+    const nome = document.getElementById('nomeCompleto').value;
+    const cpf = document.getElementById('cpfCadastro').value;
+    const agencia = document.getElementById('agencia').value;
+    const conta = document.getElementById('Conta').value;
+    const senha = document.getElementById('senhaCadastro').value;
+    const confirmaSenha = document.getElementById('confirmaSenha').value;
+
+
+
+
+    if (nome === "" || cpf === "" || conta=== "" |agencia === ""|| senha === "" || confirmaSenha === "") {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Atenção!',
+            text: 'Por favor, preencha todos os campos.',
+            });
+    } else if (senha !== confirmaSenha) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Erro de Senha',
+            text: 'As senhas não coincidem. Tente novamente.',
+        });
+    } else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Cadastro Bem-Sucedido!',
+            text: 'Você será redirecionado em breve.',
+            showConfirmButton: false,
+            timer: 1500
+        }).then(() => {
+            window.location.href ="./login.html";
+        });
+    }
+});
