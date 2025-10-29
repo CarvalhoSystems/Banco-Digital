b7d5378534356a305755b3daa83fc2e3e590db36
Rodrigo de Carvalho <carvalho_borges@icloud.com>
2025-10-29 14:27:14 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -161,7 +161,7 @@ document.getElementById("sair").addEventListener("click", () => {
     confirmButtonText: "Sim, sair!",
   }).then((result) => {
     if (result.isConfirmed) {
-      window.location.href = "/indexLanpage.html";
+      window.location.href = "./index.html";
     }
   });
 });
